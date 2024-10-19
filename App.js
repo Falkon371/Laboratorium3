d88b34397a9d732041fcb61a97587becf3d01310
Konrad Falkowski <konradfalkow13@gmail.com>
2024-10-19 09:47:52 +0200

    Zadanie 2

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { TouchableOpacity ,StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
+
+  const [isVisible, setIsVisible] = useState(false);
+  const [text, setText] = useState('Pokaż')
+
+  const toggleVisibilityText = () =>{
+    setIsVisible(!isVisible);
+    setText(prevText => prevText === 'Pokaż' ? 'Ukryj' : 'Pokaż');
+
+  }
+
   return (
+    
+    
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={styles.tekst}>
+      <Text style={styles.zad2}>Zadanie 2</Text>
     </View>
+
+    <TouchableOpacity style={styles.button} onPress={toggleVisibilityText} >
+      <Text style={styles.buttonText}>{text}</Text>
+    </TouchableOpacity>
+
+    {isVisible && (
+      <View style={styles.nameContainer}>
+        <Text style={styles.nameText}>Nazywam się 
+        <Text style={styles.boldText}> Konrad Falkowski</Text>
+        </Text>
+        
+      </View>
+    )}
+  </View>
   );
 }
 
@@ -17,4 +47,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    width: 45,
+    height: 20,
+    borderWidth: 1,
+    backgroundColor: '#c8c8c8',
+    borderColor: 'black',
+    marginBottom: 20,
+    marginTop: 20
+  },
+  nameContainer:{
+    width: 120,
+  },
+  zad2:{
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  boldText:{
+    fontWeight: 'bold',
+  }
+  
 });
