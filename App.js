5a0add8c3823ba45a6e0a9ed8c7f0a0a0741c0e0
Konrad Falkowski <konradfalkow13@gmail.com>
2024-10-19 20:15:27 +0200

    Calculator

--- App.js
+++ App.js
@@ -5,37 +5,100 @@ import { TouchableOpacity ,StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
-  const [isVisible, setIsVisible] = useState(false);
-  const [text, setText] = useState('Pokaż')
+  const [text, setText] = useState('');
 
-  const toggleVisibilityText = () =>{
-    setIsVisible(!isVisible);
-    setText(prevText => prevText === 'Pokaż' ? 'Ukryj' : 'Pokaż');
+  const dodajText = (val) =>{
+    setText(prevVal => prevVal + val)
+  }
 
+  const clearAll = () =>{
+    setText('')
+  }
+    
+  const wynik = (t) => {
+    const res = eval(t);
+    setText(res)
   }
 
   return (
-    
-    
+  <View style={styles.container}>
 
-    <View style={styles.container}>
-
-    <View style={styles.tekst}>
-      <Text style={styles.zad2}>Zadanie 2</Text>
+    <View style={styles.textScreen}>
+    <Text style={styles.obliczenia}>{text}</Text>
     </View>
 
-    <TouchableOpacity style={styles.button} onPress={toggleVisibilityText} >
-      <Text style={styles.buttonText}>{text}</Text>
+    <View style={styles.buttons}>
+    <TouchableOpacity onPress={() => clearAll()}>
+    <Text style={styles.button}>AC</Text>
+    </TouchableOpacity>
+    <Text style={styles.buttonWide}></Text>
+    
+    <TouchableOpacity onPress={() => dodajText(' / ')}>
+    <Text style={styles.operatory}>/</Text>
     </TouchableOpacity>
 
-    {isVisible && (
-      <View style={styles.nameContainer}>
-        <Text style={styles.nameText}>Nazywam się 
-        <Text style={styles.boldText}> Konrad Falkowski</Text>
-        </Text>
-        
-      </View>
-    )}
+
+    <TouchableOpacity onPress={() => dodajText('7')}>
+    <Text style={styles.button}>7</Text>
+    </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('8')}>
+      <Text style={styles.button}>8</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('9')}>
+      <Text style={styles.button}>9</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText(' * ')}>
+      <Text style={styles.operatory}>X</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('4')}>
+      <Text style={styles.button}>4</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('5')}>
+      <Text style={styles.button}>5</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('6')}>
+      <Text style={styles.button}>6</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText(' - ')}>
+      <Text style={styles.operatory}>-</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('1')}>
+      <Text style={styles.button}>1</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('2')}>
+      <Text style={styles.button}>2</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('3')}>
+      <Text style={styles.button}>3</Text>
+      </TouchableOpacity>
+      
+      <TouchableOpacity onPress={() => dodajText(' + ')}>
+      <Text style={styles.operatory}>+</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('0')}>
+      <Text style={styles.buttonWide}>0</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => dodajText('.')}>
+      <Text style={styles.button}>.</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => wynik(text)}>
+      <Text style={styles.operatory}>=</Text>
+      </TouchableOpacity>
+    </View>
+    
   </View>
   );
 }
@@ -47,24 +110,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttons:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    padding: 5,
+  },
   button:{
-    width: 45,
-    height: 20,
-    borderWidth: 1,
-    backgroundColor: '#c8c8c8',
-    borderColor: 'black',
-    marginBottom: 20,
-    marginTop: 20
+    textAlign: 'center', 
+    textAlignVertical: 'center',
+    width: 100,
+    height: 90,
+    color: 'white',
+    fontSize: 30,
+    backgroundColor: '#646464'
   },
-  nameContainer:{
-    width: 120,
+  buttonWide:{
+    textAlign: 'center', 
+    textAlignVertical: 'center',
+    width: 200,
+    height: 90,
+    color: 'white',
+    fontSize: 30,
+    backgroundColor: '#646464'
   },
-  zad2:{
-    fontWeight: 'bold',
-    fontSize: 20
+  textScreen:{
+    backgroundColor: '#505050',
+    width: 400,
+    height: 100,
+    alignItems: 'flex-end',
+    justifyContent: 'center'
   },
-  boldText:{
-    fontWeight: 'bold',
+  obliczenia:{
+    color: 'white',
+    fontSize: 60
+  },
+  operatory:{
+    textAlign: 'center', 
+    textAlignVertical: 'center',
+    width: 100,
+    height: 90,
+    color: 'white',
+    fontSize: 30,
+    backgroundColor: '#c68308'
   }
   
 });
